3dbe6727b9711505a193cb869af11d40bf03e55c
조경진 <kj_cho@idealidea.co.kr>
2020-08-10 21:36:57 +0900

    메인화면에 리스트뷰 배치 + 학생 정보 데이터 클래스 추가

---
+++ app/src/main/java/kr/co/tjoeun/listview_20200810/datas/Student.kt
@@ -0,0 +1,12 @@
+package kr.co.tjoeun.listview_20200810.datas
+
+class Student(
+    val name: String,
+    val address: String,
+    val isMale: Boolean,
+    val birthYear: Int
+) {
+//    학생이 갖는 하위 정보 (ex. 이름 / 거주지 / 성별 / 생년 등) 목록을
+//    멤버변수로 만들자. => 생성자에서 멤버변수까지 동시에 세팅.
+
+}
